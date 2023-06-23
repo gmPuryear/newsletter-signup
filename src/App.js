28c89b7cb0052b5d26a0fd9fb89034f84858b698
George Puryear <gm.puryear@yahoo.com>
2023-06-22 22:58:12 -0500

    focused on padding and margin for various elements

--- src/App.js
+++ src/App.js
@@ -10,17 +10,20 @@ function App() {
   <div className="App">
       {/* <!-- Sign-up form start --> */}
       <img className='mobile_banner' src={mobileBanner} alt='email banner'/>
+      
       <div className='form_container'>
         <h1 className="stay_updated_header">Stay updated!</h1>
 
-          <p>Join 60,000+ product managers receiving monthly updates on:</p>
+          <p className='intro_to_list'>Join 60,000+ product managers receiving monthly updates on:</p>
 
           <ul className='benefits_list'>
             <li>Product discovery and building what matters</li>
             <li>Measuring to ensure updates are a success</li>
             <li>And much more!</li>
           </ul>
-          <UserEmailInput placeHolderText = {placeHolderText}/>     
+
+            <UserEmailInput placeHolderText = {placeHolderText}/>
+
       </div>
 
   
