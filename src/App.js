849101eff3d0aa2386daebf09a3b6d99c85dc7a0
George Puryear <gm.puryear@yahoo.com>
2023-06-26 22:01:24 -0500

    finished styling mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { createContext, useEffect, useState } from 'react';
 import UserEmailInput from './components/UserEmailInput';
 import mobileBanner from './Images/illustration-sign-up-mobile.svg'
 
@@ -8,7 +7,6 @@ function App() {
 
   return (
   <div className="App">
-      {/* <!-- Sign-up form start --> */}
       <img className='mobile_banner' src={mobileBanner} alt='email banner'/>
       
       <div className='form_container'>
@@ -23,17 +21,12 @@ function App() {
           </ul>
 
             <UserEmailInput placeHolderText = {placeHolderText}/>
-
+            <div className="attribution">
+              Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+              Coded by <a href="https://www.linkedin.com/in/george-puryear/" target="_blank">George Puryear</a>.
+            </div>
       </div>
-
-  
-
-  {/* <!-- Sign-up form end --> */}
-  <div className="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">George Puryear</a>.
   </div>
-</div>
   );
 }
 
--- src/components/SuccessModal.js
+++ src/components/SuccessModal.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import {ReactComponent as SuccessCheck} from '../Images/icon-success.svg';
 
 const SuccessModal = (props) => {
         const {
@@ -9,17 +9,18 @@ const SuccessModal = (props) => {
 
     return (
         <div className="successModal_container">
+            <div className='success_Modal_top_content'>
+            <SuccessCheck/>
             {/* <!-- Success message start --> */}
-            Thanks for subscribing!
-
-A confirmation email has been sent to {userEmail}. 
-Please open it and click the button inside to confirm your subscription.
-
-<button onClick={() => setToggleSuccessModal(false)}>Dismiss message</button>
-
-{/* <!-- Success message end --> */}
-
-
+            <h1>Thanks for subscribing!</h1>
+                <p>
+                    A confirmation email has been sent to <span className='user_email'>{userEmail}</span>. 
+                    Please open it and click the button inside to confirm your subscription.
+                </p>
+            </div>
+            <div className='dismiss_btn_container'>
+                <button className='dismiss_btn' onClick={() => setToggleSuccessModal(false)}>Dismiss message</button>
+            </div>
         </div>
     )
 
