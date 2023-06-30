1e1dbd85d3c04ee5123182beaba20214c0f0d719
George Puryear <gm.puryear@yahoo.com>
2023-06-30 13:35:14 -0500

    fixed margins on tablet styles

--- src/App.js
+++ src/App.js
@@ -7,26 +7,27 @@ function App() {
 
   return (
   <div className="App">
-    <div className='form_container'>
+    <div className='container'>
       <img className='mobile_banner' src={mobileBanner} alt='email banner'/>
       
-      
-        <h1 className="stay_updated_header">Stay updated!</h1>
+      <div className='form_container'>
+          <h1 className="stay_updated_header">Stay updated!</h1>
 
-          <p className='intro_to_list'>Join 60,000+ product managers receiving monthly updates on:</p>
+            <p className='intro_to_list'>Join 60,000+ product managers receiving monthly updates on:</p>
 
-          <ul className='benefits_list'>
-            <li>Product discovery and building what matters</li>
-            <li>Measuring to ensure updates are a success</li>
-            <li>And much more!</li>
-          </ul>
+            <ul className='benefits_list'>
+              <li>Product discovery and building what matters</li>
+              <li>Measuring to ensure updates are a success</li>
+              <li>And much more!</li>
+            </ul>
 
-            <UserEmailInput placeHolderText = {placeHolderText}/>
-            <div className="attribution">
-              Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-              Coded by <a href="https://www.linkedin.com/in/george-puryear/" target="_blank">George Puryear</a>.
-            </div>
-      </div>
+              <UserEmailInput placeHolderText = {placeHolderText}/>
+              <div className="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                Coded by <a href="https://www.linkedin.com/in/george-puryear/" target="_blank">George Puryear</a>.
+              </div>
+        </div>
+    </div>
   </div>
   );
 }
