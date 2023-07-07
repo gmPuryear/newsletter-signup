187e47d842435b46688d4c2e6a189201d57361e0
George Puryear <gm.puryear@yahoo.com>
2023-07-07 13:43:54 -0500

    applying styling to tablet design. Running into problem with setting the height of the container

--- src/App.js
+++ src/App.js
@@ -6,29 +6,27 @@ function App() {
  const placeHolderText = "email@company.com";
 
   return (
-  <div className="App">
     <div className='container'>
       <img className='mobile_banner' src={mobileBanner} alt='email banner'/>
       
       <div className='form_container'>
-          <h1 className="stay_updated_header">Stay updated!</h1>
+        <h1 className="stay_updated_header">Stay updated!</h1>
 
-            <p className='intro_to_list'>Join 60,000+ product managers receiving monthly updates on:</p>
+        <p className='intro_to_list'>Join 60,000+ product managers receiving monthly updates on:</p>
 
-            <ul className='benefits_list'>
-              <li>Product discovery and building what matters</li>
-              <li>Measuring to ensure updates are a success</li>
-              <li>And much more!</li>
-            </ul>
+        <ul className='benefits_list'>
+          <li>Product discovery and building what matters</li>
+          <li>Measuring to ensure updates are a success</li>
+          <li>And much more!</li>
+        </ul>
 
-              <UserEmailInput placeHolderText = {placeHolderText}/>
-              <div className="attribution">
-                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-                Coded by <a href="https://www.linkedin.com/in/george-puryear/" target="_blank">George Puryear</a>.
-              </div>
+        <UserEmailInput placeHolderText = {placeHolderText}/>
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="https://www.linkedin.com/in/george-puryear/" target="_blank">George Puryear</a>.
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 
